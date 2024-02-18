193ebe23841af8bf1fd862a32a61a136628ade2a
swamyappu <swamy.yalamanchili@gmail.com>
2024-02-18 13:01:44 -0600

    comit

---
+++ mywork/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/FirApp/mywork/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ mywork/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comFirApp/mywork/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
