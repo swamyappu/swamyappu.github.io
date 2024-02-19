ca6c98e2bffcdc9c000db5357230a0a403714e54
swamyappu <swamy.yalamanchili@gmail.com>
2024-02-18 18:57:14 -0600

    Add files via upload

---
+++ controller/View1.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("com.sampleSample.controller.View1", {
+	
+	alert: function(){
+		alert("Button Pressed");
+	}
+	
+	
+	});
+});
